202f59309969f6ce65dd448b4788aaba29826508
Lucas <88864398+Lum000@users.noreply.github.com>
2023-08-19 00:20:03 -0300

    alteracao 1

--- source/php/changeSessionvalues.php
+++ source/php/changeSessionvalues.php
@@ -12,10 +12,15 @@ if(isset($_POST['closeOrder'])){
     try{
         mysqli_query($con,'CREATE DATABASE IF NOT EXISTS orders');
         mysqli_select_db($con,'orders');
-        mysqli_query($con,"CREATE TABLE IF NOT EXISTS order('name' VARCHAR(255) NOT NULL,'date' DATETIME,'value' DOUBLE,'id' INT AUTO_INCREMENT PRIMARY KEY)");
+        mysqli_query($con, 'CREATE TABLE IF NOT EXISTS orders (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            date DATETIME,
+            value DOUBLE
+        )');        
         $value = $_SESSION['value'];
         $name = $_SESSION['user'];
-        mysqli_query($con,"INSERT INTO order(name,value) VALUES($name,$value)");
+        mysqli_query($con,"INSERT INTO orders(name,value) VALUES($name,$value)");
     }
     catch (Exception $e){
         echo $e;
